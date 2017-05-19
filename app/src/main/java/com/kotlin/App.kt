ce14404d772790779a71db4bd89c99e79bf83a17
yaoyao1 <yaoyao1@staff.sina.com.cn>
2017-05-19 19:03:39 +0800

    MainActivity修改

---
+++ app/src/main/java/com/kotlin/App.kt
@@ -0,0 +1,11 @@
+package com.kotlin
+
+import android.app.Application
+
+/**
+ * Created by kan212 on 17/5/19.
+ */
+class App : Application(){
+
+
+}
---
+++ app/src/main/java/com/kotlin/ui/activity/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.kotlin.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
